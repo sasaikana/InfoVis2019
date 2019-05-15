bd0b595e9c078a0ab0c1d202e0103004a4cfba28
笹井　香菜 <192x106x@istc-a-028.epc.center.kobe-u.ac.jp>
2019-05-15 14:14:02 +0900

    kadai

--- W08/w08_main_task02.js
+++ W08/w08_main_task02.js
@@ -43,9 +43,9 @@ function main()
     {
         var S = i / 255.0; // [0,1]
         //var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
-        var R = 255;
-        var G = Math.max( Math.cos( (0.5 * S - 0.5) * Math.PI ), 0.0 );
-        var B = Math.max( Math.cos( (0.5 * S - 0.5) * Math.PI ), 0.0 );
+        var R = 1.0;
+        var G = Math.max( Math.cos( 0.5 * S * Math.PI ), 0.0 );
+        var B = Math.max( Math.cos( 0.5 * S * Math.PI ), 0.0 );
         var color = new THREE.Color( R, G, B );
         cmap.push( [ S, '0x' + color.getHexString() ] );
     }
